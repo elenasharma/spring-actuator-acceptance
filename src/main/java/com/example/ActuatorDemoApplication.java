023a5ca2456acd1fb433f1abfb5143e9e9a50211
Elena Sharma <esharma@pivotal.io>
2016-10-17 16:28:51 -0700

    Allow all origin requests with updated cors configuration
    Signed-off-by: Jonathan Berney <jberney@pivotal.io>

--- src/main/java/com/example/ActuatorDemoApplication.java
+++ src/main/java/com/example/ActuatorDemoApplication.java
@@ -2,6 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class ActuatorDemoApplication {
@@ -9,4 +13,20 @@ public class ActuatorDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ActuatorDemoApplication.class, args);
 	}
+
+	@Configuration
+	@EnableWebSecurity
+	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.authorizeRequests()
+					.antMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
+					.anyRequest().permitAll()
+					.and()
+					.antMatcher("/**").httpBasic();
+		}
+	}
+
 }
