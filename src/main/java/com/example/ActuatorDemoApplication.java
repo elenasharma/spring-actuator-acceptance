8ed3d8a392661b6230a73cea02f492f2d649089a
Elena Sharma <esharma@pivotal.io>
2016-10-18 14:19:15 -0700

    Remove security dependency
    Signed-off-by: Jonathan Berney <jberney@pivotal.io>

--- src/main/java/com/example/ActuatorDemoApplication.java
+++ src/main/java/com/example/ActuatorDemoApplication.java
@@ -3,9 +3,6 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class ActuatorDemoApplication {
@@ -13,20 +10,4 @@ public class ActuatorDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ActuatorDemoApplication.class, args);
 	}
-
-	@Configuration
-	@EnableWebSecurity
-	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http
-					.authorizeRequests()
-					.antMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
-					.anyRequest().permitAll()
-					.and()
-					.antMatcher("/**").httpBasic();
-		}
-	}
-
 }
